db523d4b2c8a570ece66225ce28aaa5d1030269d
2crt012 <2crt012@gmail.com>
2026-06-12 19:14:14 +0200

    Organized everything into functions

--- main.py
+++ main.py
@@ -1,27 +1,27 @@
+# Config
+# Path to your Stockfish executable
+pathToStockfish = ("/home/two0ort/Applications/stockfish/stockfish-ubuntu-x86-64-avx2")
+
 from stockfish import Stockfish
 
-eloMin = 1320
-eloMax = 3190
-sfElo = 0
+global sfELo
 
-# Configuration
-while sfElo < eloMin or sfElo > eloMax:
-    sfElo = int(input("Set Stockfish's Elo: "))
-    if sfElo < eloMin:
-        print("The minimum Elo is 1320.")
-    elif sfElo > eloMax:
-        print("The maximum Elo is 3190.")
-    else:
-        pass
- 
-sf = Stockfish(path="/home/two0ort/Applications/stockfish/stockfish-ubuntu-x86-64-avx2")
-sf.set_elo_rating(sfElo)
-
-# Show board
-print(sf.get_board_visual())
-
-# Game loop
-while True:
+# Asks user to set the Elo rating of the Fish
+def setElo():
+    global eloMin, eloMax, sfElo
+    eloMin = 1320
+    eloMax = 3190
+    sfElo = 0
+    while sfElo < eloMin or sfElo > eloMax:
+        sfElo = int(input("Set Stockfish's Elo: "))
+        if sfElo < eloMin:
+            print("The minimum Elo is 1320.")
+        elif sfElo > eloMax:
+            print("The maximum Elo is 3190.")
+        else:
+            pass
+# Game Loop, run constantly after settings
+def gameLoop():
     # Ask for move
     print("Enter move: ")
     myMove = input()
@@ -38,3 +38,11 @@ while True:
     except ValueError:
         # Handle the exception here
         print("That move isn't legal, try again!")
+
+sf = Stockfish(path=pathToStockfish)
+setElo()
+sf.set_elo_rating(sfElo)
+
+print(sf.get_board_visual())
+while True:
+    gameLoop()
